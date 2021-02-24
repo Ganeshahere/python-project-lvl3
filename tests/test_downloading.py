2371ccda199bfe41dc96c24fc0c6784ecb4cb20b
Ganesha_here <Jamper1488@gmail.com>
2021-02-24 17:50:24 +0300

    another

--- tests/test_downloading.py
+++ tests/test_downloading.py
@@ -7,7 +7,8 @@ from page_loader.loading import download
 import requests_mock
 
 
-with open('fixtures/page_with_local_links.html', 'r') as file:
+with open(os.path.abspath(os.path.join(os.pardir, 
+    'fixtures/page_with_local_links.html', 'r') as file:
     expected_page = file.read()
 
 
